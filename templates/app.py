9720df772b89d0ab4260bf776205b8dc7643cd26
lshgrinrgs-hue <lshgrinrgs@gmail.com>
2025-10-28 16:37:34 +0330

    Add files via upload

---
+++ templates/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return send_file("student.html")
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=5000, debug=True)
